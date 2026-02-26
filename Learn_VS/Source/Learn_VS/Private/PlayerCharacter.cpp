b9e9ba2fbd07d87cd4f9969a966d187ee9709a98
JusTH <164820528+JinHui123123@users.noreply.github.com>
2026-02-26 23:28:55 +0800

    新增c++学习
    绑定角色的输入映射文档+获取到角色输入

--- Learn_VS/Source/Learn_VS/Private/PlayerCharacter.cpp
+++ Learn_VS/Source/Learn_VS/Private/PlayerCharacter.cpp
@@ -3,21 +3,87 @@
 
 #include "PlayerCharacter.h"
 
-APlayerCharacter::APlayerCharacter()
+#include "GameFramework/SpringArmComponent.h" //game framwork用来添加摄像机臂组件的头文件
+#include "Camera/CameraComponent.h"
+
+#include "EnhancedInputSubsystems.h" //EnhancedInputSubsystem 是UE5中用于处理输入系统的头文件，包含了输入映射上下文、输入动作等相关类的定义
+#include "EnhancedInputComponent.h" //EnhancedInputComponent 是UE5中用于处理输入组件的头文件，包含了输入组件的定义和相关函数的声明
+#include "InputActionValue.h" //InputActionValue 是UE5中用于处理输入动作值的头文件，包含了输入动作值的定义和相关函数的声明
+#include "GameFramework/PlayerController.h" //PlayerController 是UE5中用于处理玩家控制器的头文件，包含了玩家控制器的定义和相关函数的声明
+	
+
+
+APlayerCharacter::APlayerCharacter() //定义玩家角色的构造函数
 {
+	//添加摄像机组件
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+
+	//将摄像机臂组件附加到根组件上
+	CameraBoom->SetupAttachment(RootComponent); //RootComponent是角色的根组件，默认是一个胶囊组件
+
+	//设置摄像机臂组件的长度
+	CameraBoom->TargetArmLength = 500.0f;
+
+	//添加摄像机组件
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+
+	//将摄像机组件附加到摄像机臂组件上
+	Camera->SetupAttachment(CameraBoom);
+
+	
+
 }
 
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();//调用父类的BeginPlay函数
+
+	//获取玩家控制器
+	APlayerController* PlayerController = Cast<APlayerController>(GetController()); //GetController()函数是APawn类中的一个成员函数，用于获取当前角色的控制器。Cast<APlayerController>()函数是UE4中的一个模板函数，用于将一个对象转换为指定类型的指针，如果转换成功则返回该指针，否则返回nullptr。
+
+	//如果玩家控制器存在，则获取输入子系统并添加输入映射上下文
+	if(PlayerController)
+	{
+		//获取输入子系统
+		//UEnhancedInputLocalPlayerSubsystem 是UE5中用于处理输入系统的子系统类，提供了添加和移除输入映射上下文等功能。ULocalPlayer::GetSubsystem<T>()函数是UE4中的一个模板函数，用于获取指定类型的子系统实例，如果该子系统不存在则会创建一个新的实例并返回。
+		UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()); // PlayerController->GetLocalPlayer()函数是APlayerController类中的一个成员函数，用于获取当前玩家控制器所属的本地玩家对象。ULocalPlayer是UE4中的一个类，表示一个本地玩家对象，包含了玩家的输入、HUD、网络等相关信息。
+		
+		//如果输入子系统存在，则添加输入映射上下文
+		if (Subsystem)
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0); //AddMappingContext()函数是UEnhancedInputLocalPlayerSubsystem类中的一个成员函数，用于添加一个输入映射上下文到输入系统中。DefaultMappingContext是APlayerCharacter类中的一个成员变量，表示默认的输入映射上下文。第二个参数0表示该输入映射上下文的优先级，数值越小优先级越高。
+		} 
+	}
 }
+//->是C++中的成员访问运算符，用于访问指针所指向对象的成员函数或成员变量。
+
+void APlayerCharacter::Move(const FInputActionValue & value)
+{
+	UE_LOG(LogTemp, Display, TEXT("Run Move Function"));
+}
+
+void APlayerCharacter::Look(const FInputActionValue& value)
+{
+
+}
+
 
 void APlayerCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);//调用父类的Tick函数
 }
 
-void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) // SetupPlayerInputComponent ()函数是AActor类中的一个虚函数，用于设置角色的输入组件。
 {
+	//调用父类的SetupPlayerInputComponent函数
+	UEnhancedInputComponent* MyInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent); 
+	// MyInputComponent是一个指向UEnhancedInputComponent类型的指针，用于存储输入组件的实例。Cast<UEnhancedInputComponent>()函数是UE4中的一个模板函数，用于将一个对象转换为指定类型的指针，如果转换成功则返回该指针，否则返回nullptr。
+	if (MyInputComponent)
+	{
+		MyInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Jump);
 
+		MyInputComponent->BindAction(MoveAction,ETriggerEvent::Triggered,this,&APlayerCharacter::Move);
+//BindAction()函数是UEnhancedInputComponent类中的一个成员函数，用于绑定一个输入动作到一个函数。MoveAction是APlayerCharacter类中的一个成员变量，表示移动输入动作。ETriggerEvent::Triggered表示当输入动作被触发时调用绑定的函数。this表示当前对象的指针。&APlayerCharacter::Move表示要绑定的函数指针，指向APlayerCharacter类中的Move函数。
+
+	}
 }
--- Learn_VS/Source/Learn_VS/Public/PlayerCharacter.h
+++ Learn_VS/Source/Learn_VS/Public/PlayerCharacter.h
@@ -6,6 +6,17 @@
 #include "BaseCharacter.h"
 #include "PlayerCharacter.generated.h"
 
+class USpringArmComponent; //声明摄像机臂组件
+class UCameraComponent;//声明摄像机组件
+
+//UInputMappingContext用来定义输入映射上下文，它包含了一组输入动作和它们的绑定关系，可以在游戏中动态添加或移除输入映射上下文，以实现不同的输入配置。
+class UInputMappingContext; 
+
+//UInputAction用来定义输入动作，它表示玩家可以执行的某个操作，例如移动、跳跃、攻击等。每个输入动作可以绑定一个或多个输入事件，例如按键、鼠标点击、手柄按钮等。当玩家触发这些输入事件时，输入动作会被执行，从而实现相应的游戏功能。
+class UInputAction; 
+
+//FInputActionValue用来定义输入动作的值，它表示玩家在执行输入动作时所提供的输入数据，例如按键的状态、鼠标的位置、手柄的摇杆方向等。每个输入动作可以有一个或多个输入值，这些输入值可以在游戏中被读取和处理，以实现不同的游戏功能。
+struct FInputActionValue;  //struct是C++中的一个关键字，用于定义一个结构体类型。结构体是一种用户定义的数据类型，可以包含多个成员变量，这些成员变量可以是不同类型的数据。结构体通常用于表示一些相关的数据集合，例如一个玩家角色的属性、一个游戏物体的位置和旋转等。在UE4中，FInputActionValue是一个结构体类型，用于表示输入动作的值，包含了输入数据的相关信息，例如按键的状态、鼠标的位置、手柄的摇杆方向等。
 /**
  * 
  */
@@ -20,6 +31,30 @@ class LEARN_VS_API APlayerCharacter : public ABaseCharacter
 	
 	protected:
 		virtual void BeginPlay() override;
+
+
+		UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+		USpringArmComponent* CameraBoom;//摄像机臂组件
+
+		UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+		UCameraComponent* Camera; //摄像机组件
+
+		//用于定义持有相关模块成员
+
+		UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "MyInput")
+		UInputMappingContext* DefaultMappingContext; //输入映射上下文
+
+		UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "MyInput")
+		UInputAction* MoveAction; //移动输入动作
+
+		UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "MyInput")
+		UInputAction* LookAction; //查看输入动作
+
+		UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "MyInput")
+		UInputAction* JumpAction; //跳跃输入动作
+
+		void Move(const FInputActionValue& Value); //移动函数
+		void Look(const FInputActionValue& Value); //查看函数
 
 	public:
 		virtual void Tick(float DeltaTime) override;
